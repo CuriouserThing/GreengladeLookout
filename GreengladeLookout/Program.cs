e8430d11b5d6688a1fb2b94c076dd4119e86996f
TheMostCuriousThing <adelaidekwalter@gmail.com>
2020-07-14 16:35:02 -0400

    Add project files.

---
+++ GreengladeLookout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreengladeLookout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
